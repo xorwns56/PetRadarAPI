e46c3b0e27a1ee53dbf78c444d64b0fc793a3498
xorwns <xorwns56@naver.com>
2025-09-03 03:30:57 +0900

    SSE 추가

--- src/main/java/com/example/PetRadar/notification/NotificationService.java
+++ src/main/java/com/example/PetRadar/notification/NotificationService.java
@@ -1,24 +1,31 @@
 package com.example.PetRadar.notification;
 
 import com.example.PetRadar.missing.MissingDTO;
+import com.example.PetRadar.sse.SseRepository;
 import com.example.PetRadar.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class NotificationService {
-    NotificationRepository notificationRepository;
-    UserRepository userRepository;
+    private final NotificationRepository notificationRepository;
+    private final UserRepository userRepository;
+    private final SseRepository sseRepository;
 
     public void createNotification(Long userId, String postType, Long postId) {
         Notification notification = new Notification();
         notification.setUser(userRepository.getReferenceById(userId));
         notification.setPostType(postType);
         notification.setPostId(postId);
+
+        //여기서 sendNewPostNotification 추가
+
         notificationRepository.save(notification);
     }
 
@@ -31,5 +38,19 @@ public class NotificationService {
                         notification.getPostId()
                 )
         ).collect(Collectors.toList());
+    }
+
+    public void sendNewPostNotification(Long userId, String message) {
+        SseEmitter emitter = sseRepository.get(userId);
+        if (emitter != null) {
+            try {
+                emitter.send(SseEmitter.event()
+                        .name("newPost")
+                        .data(message));
+            } catch (IOException e) {
+                emitter.completeWithError(e);
+                sseRepository.delete(userId);
+            }
+        }
     }
 }
